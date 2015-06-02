99ace6a4850e66711de194115d32ac7fd20445f4
Aravind K N <aravikn@gmail.com>
2015-06-02 19:27:56 +0530

    Update template.clj
    Fixed the indentation issues

--- module-check/src/test/clojure/clojure/core/typed/test/template.clj
+++ module-check/src/test/clojure/clojure/core/typed/test/template.clj
@@ -1,7 +1,7 @@
 (ns test.template
-(:require [clojure.core.typed :as t]
-          [clojure.template :as template]
-                [test.core]))
+  (:require [clojure.core.typed :as t]
+            [clojure.template :as template]
+            [test.core]))
 
 
 (template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5))

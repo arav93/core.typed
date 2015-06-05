59b6bfbda7d4220ee2fd30cc636edfba002234e4
Aravind K N <aravikn@gmail.com>
2015-06-05 19:03:38 +0530

    Update template.clj
    Added is-tc-e and is-tc-err tests

--- module-check/src/test/clojure/clojure/core/typed/test/template.clj
+++ module-check/src/test/clojure/clojure/core/typed/test/template.clj
@@ -1,13 +1,11 @@
-(ns test.check
+(ns clojure.core.typed.test.template
   (:require 	[clojure.core.typed :as t]
-              [clojure.template :as template] 
-		          [clojure.xml :as xml]
-		          [clojure.zip :as zip]
-		          [clojure.test :refer :all]                
-		          [clojure.core.typed.test.test-utils :refer [is-tc-e]]))
-
+                [clojure.template :as template] 
+		[clojure.xml :as xml]
+		[clojure.zip :as zip]
+		[clojure.test :refer :all]                
+		[clojure.core.typed.test.test-utils :refer :all]))
 
 (deftest check-test
-  (is-tc-e (apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5)) (t/Any))
-  (is-tc-e (clojure.template/do-template [x y] (+ y x) 2 4 3 5) (t/List t/Any))
-)
+(is-tc-e (clojure.template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5))t/Any) 
+(is-tc-err (clojure.template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5)) t/Any))

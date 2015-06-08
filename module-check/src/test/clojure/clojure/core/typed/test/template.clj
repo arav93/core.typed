209f7396db8288106f3cfc6e8b1d2bcc813f9675
Aravind K N <aravikn@gmail.com>
2015-06-08 23:57:02 +0530

    Update template.clj
    Added is-tc-err tests for apply-template

--- module-check/src/test/clojure/clojure/core/typed/test/template.clj
+++ module-check/src/test/clojure/clojure/core/typed/test/template.clj
@@ -6,6 +6,6 @@
 		[clojure.test :refer :all]                
 		[clojure.core.typed.test.test-utils :refer :all]))
 
-(deftest check-test
+(deftest applytemplate-test
 (is-tc-e (clojure.template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5))t/Any) 
-(is-tc-err (clojure.template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5)) t/Any))
+(is-tc-err (clojure.template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5)) t/Str))

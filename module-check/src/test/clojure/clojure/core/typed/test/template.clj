ef610f70d123cda212be7df72b331be7387c861f
Aravind K N <aravikn@gmail.com>
2015-06-09 00:38:43 +0530

    Update template.clj
    Added is-tc-e and is-tc-err tests for do-template function.

--- module-check/src/test/clojure/clojure/core/typed/test/template.clj
+++ module-check/src/test/clojure/clojure/core/typed/test/template.clj
@@ -6,6 +6,12 @@
 		[clojure.test :refer :all]                
 		[clojure.core.typed.test.test-utils :refer :all]))
 
-(deftest applytemplate-test
+(deftest applyTemplate-test
 (is-tc-e (clojure.template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5))t/Any) 
 (is-tc-err (clojure.template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5)) t/Str))
+
+(deftest doTemplate-test
+
+	(is-tc-e (clojure.template/do-template [x y] (+ y x) 2 4 3 5) t/Any)
+	(is-tc-err (clojure.template/do-template [x y] (+ y x) 2 4 3 5) t/Str))
+

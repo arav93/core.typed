ea12d34f6a6559a0ec44f5db0778d2df21541e30
Aravind K N <aravikn@gmail.com>
2015-06-10 19:15:01 +0530

    Update template.clj
    Altered the is-tc-err test

--- module-check/src/test/clojure/clojure/core/typed/test/template.clj
+++ module-check/src/test/clojure/clojure/core/typed/test/template.clj
@@ -1,5 +1,5 @@
 (ns clojure.core.typed.test.template
-  (:require 	[clojure.core.typed :as t]
+  (:require 	[clojure.core.typed :as t] 
                 [clojure.template :as template] 
 		[clojure.test :refer :all]                
 		[clojure.core.typed.test.test-utils :refer :all]))
@@ -8,7 +8,4 @@
   (is-tc-e(clojure.template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5))t/Any) 
   (is-tc-e(clojure.template/apply-template '[a b c d e] {:a 1 :b 2 :c 3 :d 4 :e 5} '(1 2 3 4 5))t/Any)
   (is-tc-e(clojure.template/apply-template '[a b c d e] 3 '(1 2 3 4 5))t/Any)
-  (is-tc-err(clojure.template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5)) (t/Vec t/Any))
-  (is-tc-err(clojure.template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5)) (t/HVec[t/Any]))
-  (is-tc-err(clojure.template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5)) (t/List t/Any)))
-
+  (is-tc-err (clojure.template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5)) (t/U (t/Vec t/Any) (t/HVec [t/Any]) (t/List t/Any)))) 

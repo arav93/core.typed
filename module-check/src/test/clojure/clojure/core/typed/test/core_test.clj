3296cbc878a63088f9d2d3b523aefb00abd4734a
Aravind K N <aravikn@gmail.com>
2015-06-10 18:45:10 +0530

    Delete core_test.clj

--- module-check/src/test/clojure/clojure/core/typed/test/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns test.core-test
-  (:require [clojure.core.typed :as t]
-            [clojure.test :refer :all]
-            [test.core]))
-
-(deftest check-test
-  (is (t/check-ns 'test.check)))

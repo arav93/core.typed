7726aaf45461a04d439cded8648c0b79dd0233dc
Aravind K N <aravikn@gmail.com>
2015-06-10 19:07:24 +0530

    Update template.clj
    Fixed a few errors

--- module-check/src/test/clojure/clojure/core/typed/test/template.clj
+++ module-check/src/test/clojure/clojure/core/typed/test/template.clj
@@ -5,17 +5,10 @@
 		[clojure.core.typed.test.test-utils :refer :all]))
 
 (deftest applyTemplate-test
-  (is-tc-e   (clojure.template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5))t/Any) 
-  (is-tc-e   (clojure.template/apply-template '[a b c d e] {:a 1 :b 2 :c 3 :d 4 :e 5} '(1 2 3 4 5))t/Any)
-  (is-tc-e   (clojure.template/apply-template '[a b c d e] 3 '(1 2 3 4 5))t/Any)
-  (is-tc-err (clojure.template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5)) (t/Vec t/Any))
-  (is-tc-err (clojure.template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5)) (t/HVec t/Any))
-  (is-tc-err (clojure.template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5)) (t/List t/Any)))
+  (is-tc-e(clojure.template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5))t/Any) 
+  (is-tc-e(clojure.template/apply-template '[a b c d e] {:a 1 :b 2 :c 3 :d 4 :e 5} '(1 2 3 4 5))t/Any)
+  (is-tc-e(clojure.template/apply-template '[a b c d e] 3 '(1 2 3 4 5))t/Any)
+  (is-tc-err(clojure.template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5)) (t/Vec t/Any))
+  (is-tc-err(clojure.template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5)) (t/HVec[t/Any]))
+  (is-tc-err(clojure.template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5)) (t/List t/Any)))
 
-
-(deftest doTemplate-test
-	(is-tc-e   (clojure.template/do-template [x y] (+ y x) 2 4 3 5) t/Any)
-	(is-tc-e   (clojure.template/do-template [x y] (* (+ x y) (- x y) ) 8 3) t/Any)
-	(is-tc-err (clojure.template/do-template [x y] (+ y x) 2 4 3 5) (t/Vec t/Any))
-	(is-tc-err (clojure.template/do-template [x y] (+ y x) 2 4 3 5) (t/HVec t/Any))
-	(is-tc-err (clojure.template/do-template [x y] (+ y x) 2 4 3 5) (t/List t/Any)))

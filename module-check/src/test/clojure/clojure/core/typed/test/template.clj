28dd0b812d01c41b16a39ee42892b98156b6daec
Aravind K N <aravikn@gmail.com>
2015-06-11 10:44:39 +0530

    Update template.clj

--- module-check/src/test/clojure/clojure/core/typed/test/template.clj
+++ module-check/src/test/clojure/clojure/core/typed/test/template.clj
@@ -5,7 +5,11 @@
 		[clojure.core.typed.test.test-utils :refer :all]))
 
 (deftest applyTemplate-test
-  (is-tc-e(clojure.template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5))t/Any) 
-  (is-tc-e(clojure.template/apply-template '[a b c d e] {:a 1 :b 2 :c 3 :d 4 :e 5} '(1 2 3 4 5))t/Any)
-  (is-tc-e(clojure.template/apply-template '[a b c d e] 3 '(1 2 3 4 5))t/Any)
-  (is-tc-err (clojure.template/apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5)) (t/U (t/Vec t/Any) (t/HVec [t/Any]) (t/List t/Any)))) 
+  (is-tc-e   (apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5))
+              :requires [[clojure.template :refer [apply-template]]]) 
+  (is-tc-e   (apply-template '[a b c d e] {:a 1 :b 2 :c 3 :d 4 :e 5} '(1 2 3 4 5))
+              :requires [[clojure.template :refer [apply-template]]])
+  (is-tc-e   (apply-template '[a b c d e] 3 '(1 2 3 4 5))
+              :requires [[clojure.template :refer [apply-template]]])  
+  (is-tc-err (apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5)) (t/U (t/Vec t/Any) (t/HVec [t/Any]) (t/List t/Any) )
+ 	      :requires [[clojure.template :refer [apply-template]]]))

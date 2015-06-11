1d0aadf2778188151ece0222846853962d94eddb
Aravind K N <aravikn@gmail.com>
2015-06-12 00:43:27 +0530

    Update set_new.clj

--- module-check/src/test/clojure/clojure/core/typed/test/set_new.clj
+++ module-check/src/test/clojure/clojure/core/typed/test/set_new.clj
@@ -1,8 +1,8 @@
 (ns clojure.core.typed.test.set_new
   (:require [clojure.core.typed :as t]
             [clojure.set :as set] 
-		        [clojure.test :refer :all]                
-		        [clojure.core.typed.test.test-utils :refer :all]))
+	    [clojure.test :refer :all]                
+            [clojure.core.typed.test.test-utils :refer :all]))
 
 (deftest subset-test
  (is-tc-e (subset? #{1} #{2}) Boolean 

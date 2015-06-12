3363255e01657d61668c6d881aec5126d5031a1d
Aravind K N <aravikn@gmail.com>
2015-06-13 02:16:50 +0530

    Create repl_new.clj

---
+++ module-check/src/test/clojure/clojure/core/typed/test/repl_new.clj
@@ -0,0 +1,29 @@
+(ns clojure.core.typed.test.set_new
+  (:require [clojure.core.typed :as t]
+            [clojure.set :as set] 
+            [clojure.test :refer :all]                
+            [clojure.core.typed.test.test-utils :refer :all]))
+
+(deftest apropos-test
+  (is-tc-e (apropos "clojure") t/Str
+           :requires [[clojure.repl :refer [apropos]]])
+  (is-tc-err (apropos "clojure") (t/Vec t/Any) 
+             :requires [[clojure.repl :refer [apropos]]])
+  (is-tc-err (apropos 'clojure) t/Str 
+             :requires [[clojure.repl :refer [apropos]]]))
+             
+(deftest demunge-test
+  (is-tc-e (demunge "clojure.repl$demunge") t/Str
+           :requires [[clojure.repl :refer [demunge]]])
+  (is-tc-err (demunge "clojure.repl$demunge") (t/Vec t/Any) 
+             :requires [[clojure.repl :refer [demunge]]])
+  (is-tc-err (demunge 'clojure.repl$demunge) t/Str 
+             :requires [[clojure.repl :refer [demunge]]]))
+             
+(deftest source-fn-test
+  (is-tc-e (source-fn 'source) t/Str
+           :requires [[clojure.repl :refer [source-fn]]])
+  (is-tc-err (source-fn 'source) (t/Vec t/Any) 
+             :requires [[clojure.repl :refer [source-fn]]])
+  (is-tc-err (source-fn "source") t/Str 
+             :requires [[clojure.repl :refer [source-fn]]]))

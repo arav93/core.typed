5d7dfb07b5424d9d59b8b777b490a2ae069111d1
Aravind K N <aravikn@gmail.com>
2015-06-14 01:33:24 +0530

    Update template.clj
    Changed name from applyTemplate to apply-template

--- module-check/src/test/clojure/clojure/core/typed/test/template.clj
+++ module-check/src/test/clojure/clojure/core/typed/test/template.clj
@@ -1,10 +1,9 @@
 (ns clojure.core.typed.test.template
   (:require  [clojure.core.typed :as t] 
-             [clojure.template :as template] 
              [clojure.test :refer :all]                
              [clojure.core.typed.test.test-utils :refer :all]))
 
-(deftest applyTemplate-test
+(deftest apply-template-test
   (is-tc-e   (apply-template '[a b c d e] '[d a b e c e b a d] '(1 2 3 4 5))
              :requires [[clojure.template :refer [apply-template]]]) 
   (is-tc-e   (apply-template '[a b c d e] {:a 1 :b 2 :c 3 :d 4 :e 5} '(1 2 3 4 5))

22f7ebddfaef1825babf5218504511c76bced022
Aravind K N <aravikn@gmail.com>
2015-06-17 16:03:38 +0530

    Update base_env.clj for clojure.data
    Added annotations for clojure.data namespace

--- module-check/src/main/clojure/clojure/core/typed/base_env.clj
+++ module-check/src/main/clojure/clojure/core/typed/base_env.clj
@@ -1445,6 +1445,8 @@ clojure.set/union (All [x] [(Set x) * -> (Set x)])
 clojure.set/intersection (All [x] [(Set x) (Set x) * -> (Set x)])
 clojure.set/difference (All [x] [(Set x) (Set Any) * -> (Set x)])
 
+ clojure.data/diff [(U (List Any ) (HVec [Any *] )) (U (List Any ) (HVec [Any *] ))->  (HVec [Any *] )]
+
 clojure.repl/pst (IFn [-> nil]
                       [(U Int Throwable) -> nil]
                       [Throwable Int -> nil])

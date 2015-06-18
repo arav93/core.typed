322f9f4e6fa04a319119de1cc33962f1354a89b6
Aravind K N <aravikn@gmail.com>
2015-06-18 21:41:50 +0530

    Update base_env.clj

--- module-check/src/main/clojure/clojure/core/typed/base_env.clj
+++ module-check/src/main/clojure/clojure/core/typed/base_env.clj
@@ -1445,7 +1445,7 @@ clojure.set/union (All [x] [(Set x) * -> (Set x)])
 clojure.set/intersection (All [x] [(Set x) (Set x) * -> (Set x)])
 clojure.set/difference (All [x] [(Set x) (Set Any) * -> (Set x)])
 
- clojure.data/diff [(U (List Any ) (Vec [Any *] ) (Map Any Any) Long) (U (List Any ) (Vec [Any *] ) (Map Any Any) Long) -> (U (Vec [Any *] ) (Seq Any))]
+ clojure.data/diff [Any Any -> (U (Vec [Any *] ) (Seq Any))]
 
 clojure.repl/pst (IFn [-> nil]
                       [(U Int Throwable) -> nil]

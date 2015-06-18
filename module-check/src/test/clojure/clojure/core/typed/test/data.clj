9b38fa128f2be666a92a4b0b228bedc4ad65ce65
Aravind K N <aravikn@gmail.com>
2015-06-18 22:24:56 +0530

    Update data.clj

--- module-check/src/test/clojure/clojure/core/typed/test/data.clj
+++ module-check/src/test/clojure/clojure/core/typed/test/data.clj
@@ -5,7 +5,7 @@
              [clojure.core.typed.test.test-utils :refer :all]))
 
 (deftest diff-test
-  (is-tc-e   (diff '[1 2 3] '[1 2]) (U (Vec [Any *] ) (Seq Any)))
+  (is-tc-e   (diff '[1 2 3] '[1 2]) (U (Vec [Any *] ) (Seq Any))
              :requires [[clojure.data :refer [diff]]]) 
   (is-tc-err   (diff '[1 2 3] '[1 2]) (t/List t/Any )
              :requires [[clojure.data :refer [diff]]]))

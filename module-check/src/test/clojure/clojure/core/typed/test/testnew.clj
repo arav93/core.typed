8d204c4f1790149de2402f4d60b3bcc155f87757
Aravind K N <aravikn@gmail.com>
2015-07-01 16:10:15 +0530

    Update testnew.clj

--- module-check/src/test/clojure/clojure/core/typed/test/testnew.clj
+++ module-check/src/test/clojure/clojure/core/typed/test/testnew.clj
@@ -3,15 +3,9 @@
              [clojure.test :refer :all]                
              [clojure.core.typed.test.test-utils :refer :all]))
 
-(deftest function?-test
-  (is-tc-e   (function? function?) Boolean
-             :requires [[clojure.test :refer [function?]]])
-  (is-tc-err   (function? function?) String
-             :requires [[clojure.test :refer [function?]]]))
-  
-(deftest assert-any-test
-  (is-tc-e   (assert-any "Hi" (= 4(+ 2 2) )) Any
-             :requires [[clojure.test :refer [assert-any]]]))
+(deftest runTest-test
+  (is-tc-e   #(run-tests) (t/Map t/Any t/Any)
+             :requires [[clojure.test :refer [run-tests]]]))
 
 
 
